6104dc2ed48cc6aedcadeb6c3e596a0e22de165f
ecstasy <shipandrewi@gmail.com>
2023-11-21 18:54:05 +0400

    CHANGE COMMIT <^-^>

--- PREFIXXdraft/mainapplication/views.py
+++ PREFIXXdraft/mainapplication/views.py
@@ -4,6 +4,5 @@ def main_page(request):
     return render(request, 'mainapplication/main_page.html')
 def about_page(request):
     return render(request,'mainapplication/about_page.html')
-
 def contacts_page(request):
     return render(request,'mainapplication/contacts_page.html')
