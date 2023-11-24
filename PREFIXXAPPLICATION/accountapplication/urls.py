0706b4b9dacefbf1fa5b544762cca2846b4199c1
ecstasy <shipandrewi@gmail.com>
2023-11-24 18:50:13 +0400

    CHANGE COMMIT <^-^>

--- PREFIXXAPPLICATION/accountapplication/urls.py
+++ PREFIXXAPPLICATION/accountapplication/urls.py
@@ -17,11 +17,4 @@ urlpatterns = [
 
     # DASHBOART PATH :
     path('', views.dashboard, name='dashboard'),
-
-    # PATH TO RESET PASSWORD :
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view, name='password_reset_confirm_view'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
 ]
